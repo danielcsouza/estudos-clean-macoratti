a22adb6ec503821574e03170d91cdc2b6c49e921
Daniel Carlos <daniel.carlos.souza@gmail.com>
2021-10-01 14:58:06 -0300

    exemplo

---
+++ CleanArch.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productAppService)
+        {
+            _productService = productAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _productService.GetProducts();
+            return View(result);
+        }
+
+        [HttpGet()]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,Description,Price")] ProductViewModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productVM = await _productService.GetById(id);
+
+            if (productVM == null) return NotFound();
+
+            return View(productVM);
+        }
+
+        [HttpPost()]
+        public IActionResult Edit([Bind("Id,Name,Description,Price")] ProductViewModel productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _productService.Update(productVM);
+                    ViewBag.Sucesso = "Produto Atualizado!";
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productVM);
+        }
+
+        // GET: ProductsVM/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productVM = await _productService.GetById(id);
+            
+            if (productVM == null)
+            {
+                return NotFound();
+            }
+
+            return View(productVM);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productVM = await _productService.GetById(id);
+
+            if (productVM == null) return NotFound();
+
+            return View(productVM);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _productService.Remove(id);
+            ViewBag.Sucesso = "Student Removed!";
+            return RedirectToAction("Index");
+        }
+    }
+}
